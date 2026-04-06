422ca358154b861468d270adb456d344d46e98db
Joy0810 <joynirantar2005@gmail.com>
2026-04-06 16:30:07 +0530

    feat: add express server entry point with auth routes

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { errorHandler } from "./middleware/error.middleware";
+import authRoutes from "./routes/auth.routes";
+
+dotenv.config();
+
+const app=express();
+const PORT=process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/health",(req,res)=>{
+    res.status(200).json({sucess:true,message:"Server is healthy"});
+})
+
+app.use("/api/auth",authRoutes);
+
+app.use(errorHandler);
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
